0936353f5661c80629c0f51a20bc857e5692c2a9
MINJO KIM <114234223+KorJM@users.noreply.github.com>
2023-03-23 15:58:09 +0900

    Add files via upload

---
+++ Stack Implementation/By LinkedList/Stack_LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+    public class Stack_LinkedList
+    {
+        static void main(string[] args)
+        {
+            StackByLinkedList<int> stack = new StackByLinkedList<int>();
+
+            stack.push(3);
+            stack.push(2);
+            stack.push(1);
+
+            Console.WriteLine(stack.peek());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.peek());
+        }
+    }
+
+    class StackByLinkedList<T>
+    {
+        private LinkedList<T> stack = new LinkedList<T>();
+
+        public void push(T value)
+        {
+            stack.AddFirst(value);
+        }
+
+        public T pop()
+        {
+            try
+            {         
+                T value = stack.First.Value;
+                stack.RemoveFirst();
+                return value;
+            }
+            catch(NullReferenceException e)
+            {
+                Console.WriteLine("Stack is Empty \nPrint default(T)");
+                return default(T);
+            }
+        }
+
+        public T peek()
+        {
+            try
+            {
+                return stack.First.Value;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Stack is Empty \nPrint default(T)");
+                return default(T);
+            }
+        }
+        
+        public int count() { return stack.Count(); }
+
+        public bool isEmpty() { return stack.Count() == 0; }
+
+        public void clear() { stack.Clear(); }
+    }
+
+
+
